f64311b3ee44fc5b238f95efa9bf7d717d3edd2d
NiteshSangroula <sangroulanitesh123@gmail.com>
2026-02-06 18:58:57 +0545

    initial commit

---
+++ src/main/java/com/example/FinanceTrackerAPI/FinanceTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.FinanceTrackerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FinanceTrackerAPI/FinanceTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FinanceTrackerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
